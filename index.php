4c05ce463c0a5ccf759f00b2270fe2cca53203e1
shokone <imartineztutor@gmail.com>
2016-01-26 17:28:59 +0100

    agregamos el archivo index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * cargamos la página
+ * @name index.php
+ * @autor Iván Martínez Tutor
+ */
+
+//validamos para mostra el home
+
+//incluimos el header
+include 'header.php';
+
+//si estamos logueados mostramos la seccion portal/mi
+//sino mostramos el home
