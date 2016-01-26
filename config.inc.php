a0385f9383c0004e677b274b15cca1c064b7fac1
shokone <imartineztutor@gmail.com>
2016-01-26 17:27:49 +0100

    agregamos config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ajustes para la base de datos
+ * guardaremos los datos en la variable $db
+ * 
+ */
+
+$db['host'] = 'host';
+$db['dabase'] = "database";
+$db['user'] = "user";
+$db['pass'] = "pass";
+
