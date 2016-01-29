9a8cf67287d81b356e9ba7f5c528eae38a5bfd47
shokone <imartineztutor@gmail.com>
2016-01-29 17:40:23 +0100

    subimos datos.php

---
+++ inc/extra/datos.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * guardaremos diferentes datos relacionados con la web
+ * meses, paises, datos de usuario, denuncias y privacidad
+ * 
+ * @name datos.php
+ * @author Iván Martínez Tutor
+ */
+/**
+ * array psMeses
+ * @funcionalidad contiene los datos en español de los meses
+ */
+$psMeses = [
+    1 => "Enero",
+    2 => "Febrero",
+    3 => "Marzo",
+    4 => "Abril",
+    5 => "Mayo",
+    6 => "Junio",
+    7 => "Julio",
+    8 => "Agosto",
+    9 => "Septiembre",
+    10 => "Octubre",
+    11 => "Noviembre",
+    12 => "Diciembre"
+];
+
+/**
+ * psPrivacidad
+ * @funcionalidad guardamos los datos de preferencia de privacidad del usuario
+ */
+$psPrivacidad = [
+    "Nadie",
+    "Usuarios que sigo y/o me siguen",
+    "Mis seguidores",
+    "Usuarios que sigo",
+    "Usuarios que est&aacute;n registrados",
+    "Todos los usuarios(visitantes tambi&eacute;n"
+];
