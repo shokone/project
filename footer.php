f65b31f14fecbdb78802af957b25595d25ffa3b7
shokone <imartineztutor@gmail.com>
2016-01-29 12:46:08 +0100

    actualizamos footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * con el footer mostramos el final de la plantilla
+ * 
+ * @name footer.php
+ * @author Iván Martínez Tutor
+ */
+
+/**
+ * realizamos las tareas necesarias para mostrar la plantilla
+ */
+
+//pagina solicitada
+$smary->assign("psPage",$psPage);
